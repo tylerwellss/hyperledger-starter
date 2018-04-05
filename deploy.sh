8fe340b13bad9193fcf58ccc26899c742dc44ff5
Tyler Wells <tyler.wellss@gmail.com>
2018-04-05 14:58:22 +0800

    add beginnings of deploy script

---
+++ deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+# Detect architecture
+ARCH=`uname -m`
+
+echo "$(tput setaf 4)$(tput bold)************************************************$(tput sgr0)"
+echo "$(tput setaf 6)$(tput bold)Deploying Hyperledger Starter. Please wait. $(tput sgr0)"
+echo "$(tput setaf 6)$(tput bold)Starting Hyperledger Fabric network ... $(tput sgr0)"
+echo "$(tput setaf 6)$(tput bold)Hyperledger Fabric v1.1 $(tput sgr0)"
+echo "$(tput setaf 4)$(tput bold)************************************************$(tput sgr0)"
+
+cd ./fabric/fabric-scripts/hlfv11/composer
+echo "$(tput setaf 6)$(tput bold)Stopping currently running Fabric in Docker$(tput sgr0)"
+ARCH=$ARCH docker-compose -f docker-compose.yml down
+
+echo "$(tput setaf 6)$(tput bold)Starting Fabric in Docker  $(tput sgr0)"
+ARCH=$ARCH docker-compose -f docker-compose.yml up -d
+
+echo "$(tput setaf 6)$(tput bold)Sleep 10 seconds while startup completes $(tput sgr0)" && sleep 10
+
+echo "$(tput setaf 6)$(tput bold)Creating Channel $(tput sgr0)" && sleep 2
+docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx
+echo "$(tput setaf 6)$(tput bold)Channel Created $(tput sgr0)"
+
+echo "$(tput setaf 6)$(tput bold)Joining Peer to Channel $(tput sgr0)" && sleep 2
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b composerchannel.block
+echo "$(tput setaf 6)$(tput bold)Peer Joined Channel $(tput sgr0)"
+
+echo "$(tput setaf 4)$(tput bold)************************************************$(tput sgr0)"
+echo "$(tput setaf 6)$(tput bold)Fabric Started! Deploying Composer network ... $(tput sgr0)"
+echo "$(tput setaf 6)$(tput bold)Creating business network archive file $(tput sgr0)"
+echo "$(tput setaf 4)$(tput bold)************************************************$(tput sgr0)"
+
+cd ../../../../composer && composer archive create -t dir -n .
+echo "$(tput setaf 6)$(tput bold)Business network archive file created $(tput sgr0)" && sleep 2
+
+echo "$(tput setaf 6)$(tput bold)Install Composer network to Fabric $(tput sgr0)"
+composer network install --card PeerAdmin@hlfv1 --archiveFile sample-network@0.0.1.bna
+echo "$(tput setaf 6)$(tput bold)Composer business network installed $(tput sgr0)" && sleep 2
+
+echo "$(tput setaf 6)$(tput bold)Start business network $(tput sgr0)"
+composer network start --networkName sample-network --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+echo "$(tput setaf 6)$(tput bold)Business network started $(tput sgr0)" && sleep 2
+
+echo "$(tput setaf 6)$(tput bold)Remove existing network admin card $(tput sgr0)"
+composer card delete -c admin@sample-network
+echo "$(tput setaf 6)$(tput bold)Existing network admin card removed $(tput sgr0)" && sleep 2
+
+
+echo "$(tput setaf 6)$(tput bold)Import network admin identity $(tput sgr0)"
+composer card import --file networkadmin.card
+echo "$(tput setaf 6)$(tput bold)Network admin identity imported $(tput sgr0)" && sleep 2
+
+echo "$(tput setaf 6)$(tput bold)Start Composer REST Server $(tput sgr0)"
+composer-rest-server -c admin@sample-network -n never
+echo "$(tput setaf 6)$(tput bold)Composer REST Server Started $(tput sgr0)" && sleep 2
